655e39a6b581d99907a8f4a500a27600b0ce2c77
whitegodz <francydona0@gmail.com>
2023-10-08 11:34:32 +0200

    Edited BigInteger.php

--- PhpAmqpLib/Helper/BigInteger.php
+++ PhpAmqpLib/Helper/BigInteger.php
@@ -2,14 +2,14 @@
 
 namespace PhpAmqpLib\Helper;
 
-if (class_exists('phpseclib\Math\BigInteger')) {
-    class BigInteger extends \phpseclib\Math\BigInteger
-    {
-    }
-} elseif (class_exists('phpseclib3\Math\BigInteger')) {
-    class BigInteger extends \phpseclib3\Math\BigInteger
-    {
-    }
-} else {
-    throw new \RuntimeException('Cannot find supported phpseclib/phpseclib library');
+use RuntimeException;
+
+if(class_exists('phpseclib\Math\BigInteger')){
+	class BigInteger extends \phpseclib\Math\BigInteger{
+	}
+}elseif(class_exists('phpseclib3\Math\BigInteger')){
+	class BigInteger extends \phpseclib3\Math\BigInteger{
+	}
+}else{
+	throw new RuntimeException('Cannot find supported phpseclib/phpseclib library');
 }
